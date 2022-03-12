44addc98ec7d6ecfcdcbdd66ea3d349921a2ee9a
Amina Miah <Miaha9@cardiff.ac.uk>
2022-03-12 13:24:56 +0000

    Introduction of the game

--- src/main/java/com/cm6123/wormhole/app/Application.java
+++ src/main/java/com/cm6123/wormhole/app/Application.java
@@ -8,25 +8,31 @@ import org.slf4j.LoggerFactory;
 import java.util.Scanner;
 
 public final class Application {
-    //create a logger for the class
-    private static final Logger logger =
-            LoggerFactory.getLogger(Application.class);
-    private static int width;
-
-    private Application() {
-        public static void main;( final String[] args){
-            logger.info("Application Started with args:{}",
-                    String.join(".", args));
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Welcome to Wormhole! \n " +
-                    "Rules: \n" +
-                    "~ 2 to 6 players allowed \n" +
-                    "~ auto roll or you can roll yourself \n" +
-                    "~ you start on tile 1 and you have to make your way to the end to win \n" +
-                    "~ watch out for the wormholes which can either send you backwards or forwards \n" +
-                    "~you get to choose how big you want the board and I will tell you where the wormholes are"); //beginning of board, welcomes you in, tells you the rules
-            System.out.println("So to start, please enter the width dimension of your board"); //asks for the width of the board
-            int width = sc.nextInt();
+    public static void main(final String[] args) {
+        System.out.println("Welcome to Wormhole! \n " +
+                "Rules: \n" +
+                "~ 2 to 6 players allowed \n" +
+                "~ auto roll or you can roll yourself \n" +
+                "~ you start on tile 1 and you have to make your way to the end to win \n" +
+                "~ watch out for the wormholes which can either send you backwards or forwards \n" +
+                "~you get to choose how big you want the board and I will tell you where the wormholes are"); //beginning of board, welcomes you in, tells you the rules
+        System.out.println("So to start, please enter the width dimension of your board"); //asks for the width of the board
+        Scanner sc = new Scanner(System.in).useDelimiter("\n");
+        while (true) {
+            int BoardWidth;
+            while(true){
+                System.out.println("Please enter the width dimension of your board: ");
+                try {
+                    BoardWidth = Integer.parseInt(sc.nextLine());
+                    if (BoardWidth >= 3 && BoardWidth <= 10) {
+                        break;
+                    } else {
+                        System.out.println("Board width needs to be between 3 and 10");
+                    }
+                } catch (Exception e) {
+                    System.out.println("Thank you");
+                }
+            }
         }
     }
 }
